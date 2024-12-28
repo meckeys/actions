ef698cf2c51d396685f9b649ac42758eced303e4
Debayan Sutradhar <debayansutradhar3@gmail.com>
2024-12-28 14:57:23 +0000

    simplify

--- deploy.sh
+++ deploy.sh
@@ -11,27 +11,19 @@ cat <<EOF
 
 Usage:
 -h | --help         print help
--l | --location     specify location
--e | --environment  specify environment variables as semi-colon separated pairs
 -n | --name         specify app name
--i | --install      install application
--u | --uninstall    delete application
--f | --force        forcefully perform operations without sanity checks
 -a | --action       specify action
 
 List of available actions:
 start           starts application, fails gracefully if already running
 stop            stops application, fails gracefully if not running
 status          prints current application status
-log             prints latest log
-error_log       prints latest error log
-delete_logs     delete all logs
 EOF
 }
 
 # Lazy evaluated properties
 app_root() {
-echo $LOCATION/$NAME
+echo $LOCATION/$APP_NAME
 }
 
 pid_file() {
@@ -39,15 +31,15 @@ echo $(app_root)/app.pid
 }
 
 app_file() {
-echo $(app_root)/$NAME.jar
+echo $(app_root)/$APP_NAME.jar
 }
 
 log_file_root() {
-echo $(app_root)/logs/$NAME
+echo $(app_root)/logs/$APP_NAME
 }
 
 log_file() {
-echo $(log_file_root)/$NAME.log
+echo $(log_file_root)/$APP_NAME.log
 }
 
 std_err_file() {
@@ -69,31 +61,15 @@ return 1
 }
 
 
-is_installed() {
-    [ -e "$(app_file)" ]
-}
-
-
 start() {
-if is_running && [ ! $FORCE ]; then
-    echo $NAME is already running with PID $(pid)
+if is_running; then
+    echo $APP_NAME is already running with PID $(pid)
     return 0
 fi
 
-if ! is_installed && [ ! $FORCE ]; then
+if [ ! -e "$(app_file)" ]; then
     echo application "$(app_file)" not found
     return 1
-fi
-
-if [ ! -z "$ENVIRONMENT" ]; then
-OLD_IFS=$IFS
-IFS=$';\n'
-for var in $ENVIRONMENT; do
-    if [[ "$var" =~  ^[a-zA-Z]+.*=.+$ ]]; then
-        declare -x "${var%%=*}=${var#*=}"
-    fi
-done
-IFS=$OLD_IFS
 fi
 
 pushd $(app_root) > /dev/null
@@ -104,10 +80,10 @@ echo $! > "$(pid_file)"
 popd > /dev/null
 
 if is_running; then
-    echo $NAME successfully started
+    echo $APP_NAME successfully started
     return 0
 else
-    echo FAILED to start $NAME
+    echo FAILED to start $APP_NAME
     return 1
 fi
 }
@@ -115,23 +91,19 @@ fi
 
 stop() {
 if ! is_running; then
-    echo $NAME is not running
+    echo $APP_NAME is not running
     return 0
 fi
 
-if [ $FORCE ]; then
-    kill -SIGKILL "$(pid)"
-else
-    kill "$(pid)"
-fi
+kill "$(pid)"
 
 sleep 5
 
 if ! is_running; then
-    echo successfully stopped $NAME
+    echo successfully stopped $APP_NAME
     return 0
 else
-    echo FAILED to stop $NAME. It may still be running.
+    echo FAILED to stop $APP_NAME. It may still be running.
     return 1
 fi
 
@@ -141,50 +113,10 @@ echo stop
 
 status() {
 if is_running; then
-echo $NAME is currently RUNNING with PID $(pid)
+    echo $APP_NAME is currently RUNNING with PID $(pid)
 else
-echo $NAME is currently NOT RUNNING
+    echo $APP_NAME is currently NOT RUNNING
 fi
-}
-
-
-open_file() {
-if [ ! -e "$1" ]; then
-    echo Failed to open "$1"
-    return 1
-fi
-
-is_running && less +F "$1" || less +G "$1"
-}
-
-
-log() {
-if [ ! -e "$(log_file)" ]; then
-echo no logs found
-return 0
-fi 
-
-if [ $(find "$(log_file_root)" -type f | grep -E "\.log$" | wc -l) -gt 1 ]; then
-    echo More log files found under "$(log_file_root)". Showing only "$(log_file)"
-fi
-
-open_file "$(log_file)" 
-}
-
-
-error_log() {
-if [ ! -e "$(std_err_file)" ]; then
-echo no error logs found
-return 0
-fi
-
-open_file "$(std_err_file)" 
-}
-
-
-delete_logs() {
-rm -rf "$(log_file_root)"
-echo Done
 }
 
 
@@ -196,27 +128,9 @@ while [ "$#" -gt 0 ]; do
             usage
             exit 0
             ;;
-        -l | --location )
-            LOCATION="$2"
-            shift
-            ;;
-        -e | --environment )
-            ENVIRONMENT="$2"
-            shift
-            ;;
         -n | --name )
-            NAME="$2"
+            APP_NAME="$2"
             shift
-            ;;
-        -i | --install )
-            PATH_TO_INSTALL="$2"
-            shift
-            ;;
-        -u | --uninstall)
-            UNINSTALL=true
-            ;;
-        -f | --force)
-            FORCE=true
             ;;
         -a | --action )
             ACTION="$2"
@@ -224,106 +138,21 @@ while [ "$#" -gt 0 ]; do
             ;;
         * )
             echo Invalid argument "$1"
-            usage
             exit 1
     esac
     shift
 done
 
-
-# Check if name is provided
-if [ -z "${NAME+x}" ]; then
-echo '-n | --name is required'
+if [ "${APP_NAME+x}${ACTION+y}" != 'xy' ]; then
+echo '-n | --name and -a | --action are required'
 exit 1
 fi
-
-
-# When not uninstalling
-if [ -z "${UNINSTALL+x}" ]; then
-
-#check if atleast install/action is provided
-if [ -z "${PATH_TO_INSTALL+y}${ACTION+z}" ]; then
-echo 'atleast -i | --install, -u | --uninstall or -a | --action is required'
-exit 1
-fi
-
-# When uninstall is used
-else
-
-# Check if uninstall and install are not used together
-if [ ! -z "${PATH_TO_INSTALL+y}" ]; then
-echo '-u | --uninstall and -i | --install cannot be used together'
-exit 1
-fi
-
-# Check if uninstall and action are not used together
-if [ ! -z "${ACTION+y}" ]; then
-echo '-u | --uninstall and -a | --action cannot be used together'
-exit 1
-fi
-
-fi
-
-
-
-# Uninstall
-if [ ! -z "${UNINSTALL+x}" ]; then
-if is_running && [ ! $FORCE ]; then
-echo $NAME is currently running. please stop it first.
-exit 1
-fi
-
-if ! is_installed && [ ! $FORCE ]; then
-echo $NAME is already not installed.
-exit 0
-fi
-
-rm -f $(app_file)
-
-echo Done
-fi
-
-
-# Install
-if [ ! -z "${PATH_TO_INSTALL+x}" ]; then
-if is_running && [ ! $FORCE ]; then
-echo $NAME is currently running. please stop it first.
-exit 1
-fi
-
-# Check if path is valid
-if ! [[ -e "${PATH_TO_INSTALL}" && "${PATH_TO_INSTALL}" =~ \.jar$ ]]; then
-echo invalid jar file
-exit 1
-fi
-
-# Delete old jar
-rm -f $(app_file)
-
-# create new directories
-mkdir -p "$(app_root)"
-
-# copy file
-cp "${PATH_TO_INSTALL}" "$(app_file)"
-
-if is_installed; then
-echo successfully installed!
-else
-echo failed to install
-exit 1
-fi
-
-fi
-
 
 # Parse action
-if [[ ! -z "$ACTION" ]]; then
-if [[ "start stop status log error_log delete_logs" =~ "$ACTION" ]]; then
+if [[ "start stop status" =~ "$ACTION" ]]; then
 "$ACTION"
 exit $?
 else
 echo invalid action $ACTION
-usage
 exit 1
-fi
 fi
